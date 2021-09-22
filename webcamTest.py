b091ff6c35d70f471ba182508b52cfb2c3a3c176
TobiasRisom <trisom19@student.aau.dk>
2021-09-22 10:48:26 +0200

    flipped image

--- webcamTest.py
+++ webcamTest.py
@@ -12,14 +12,18 @@ while (True):
     if frame is None:
         break
     frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    frame_threshold = cv2.inRange(frame_HSV, (50, 0, 0), (70, 255, 255))
+    frame_threshold = cv2.inRange(frame_HSV, (70, 1, 1), (80, 255, 255))
 
     frame_flip = cv2.flip(frame, 1)
     frame_threshold_flip = cv2.flip(frame_threshold, 1)
 
     # Display the resulting frame
-    cv2.imshow('frame', frame_flip)
     cv2.imshow('adjusted frame', frame_threshold_flip)
+
+    contours, hierarchy = cv2.findContours(frame_threshold_flip, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours = max(contours, key=lambda x: cv2.contourArea(x))
+    cv2.drawContours(frame_flip, [contours], -1, (255, 255, 0), 2)
+    cv2.imshow("contours", frame_flip)
 
     # the 'q' button is set as the
     # quitting button you may use any
