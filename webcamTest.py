c347f561b2474a022c0361c81c139bf9668f44c7
kristian1402 <ksaren20@student.aau.dk>
2021-09-22 10:11:44 +0200

    Webcamet kan detect grøn

--- webcamTest.py
+++ webcamTest.py
@@ -12,10 +12,10 @@ while (True):
     if frame is None:
         break
     frame_HSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    frame_threshold = cv2.inRange(frame_HSV, (50, 0, 0), (120, 255, 255))
+    frame_threshold = cv2.inRange(frame_HSV, (70, 190, 60), (80, 255, 255))
 
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    #cv2.imshow('frame', frame)
     cv2.imshow('adjusted frame', frame_threshold)
 
     # the 'q' button is set as the
