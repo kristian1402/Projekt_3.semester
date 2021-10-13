b6b2d71a49d48be24176a55566a2e9fe9b62f04c
TobiasRisom <trisom19@student.aau.dk>
2021-10-13 14:08:11 +0200

    average position of contour represented by circle

--- webcamTest.py
+++ webcamTest.py
@@ -6,6 +6,9 @@ green_2_test = (70, 79, 146), (85, 255, 255)
 # define a video capture object
 vid = cv2.VideoCapture(0)
 
+cX = 0
+cY = 0
+
 while (True):
     # Capture the video frame
     # by frame
@@ -20,7 +23,9 @@ while (True):
     frame_threshold_flip = cv2.flip(frame_threshold, 1)
 
     # Display the resulting frame
-    topLeft = cv2.rectangle(frame_flip, (0, 0), (150, 100), (255, 0, 0), 0)
+
+    cv2.circle(frame_flip, (cX, cY), 7, (0, 255, 255), -1)
+    cv2.rectangle(frame_threshold_flip, (0, 80), (1000, 85), (255, 0, 0), 0)
     cv2.imshow('adjusted frame', frame_threshold_flip)
 
     contours, hierarchy = cv2.findContours(frame_threshold_flip, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -28,6 +33,12 @@ while (True):
     if numpy.all(hierarchy) != None:
         contours = max(contours, key=lambda x: cv2.contourArea(x))
         cv2.drawContours(frame_flip, [contours], -1, (255, 255, 0), 2)
+
+        #Find average position of contours
+        M = cv2.moments(contours)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
     cv2.imshow("contours", frame_flip)
 
     for contours in contours:
